e38396775cc78d8436d5e1ad44218d65c5f58c10
jillianmeehan <jillianmeehan.dev@gmail.com>
2026-01-11 16:14:42 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://jillian-meehan-portfolio.netlify.app/" target="_blank">
+          Jillian Meehan
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://weather-app-react-jillian.netlify.app/"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
