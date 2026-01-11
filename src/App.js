bceb667e5167d5a2b6fa038b52c41a24bb42216c
jillianmeehan <jillianmeehan.dev@gmail.com>
2026-01-11 16:44:35 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://jillian-meehan-portfolio.netlify.app/">
-          Jillian Meehan
-        </a>{" "}
-        and is {""}
-        <a href="https://weather-app-react-jillian.netlify.app/">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://jillian-meehan-portfolio.netlify.app/">
+            Jillian Meehan
+          </a>{" "}
+          and is {""}
+          <a href="https://weather-app-react-jillian.netlify.app/">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
