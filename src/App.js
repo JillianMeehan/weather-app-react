13781420c161b33cb375981ea3e095fc0796f5dd
jillianmeehan <jillianmeehan.dev@gmail.com>
2026-01-11 16:16:34 -0500

    Removed target="blank" for now

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://jillian-meehan-portfolio.netlify.app/" target="_blank">
+        <a href="https://jillian-meehan-portfolio.netlify.app/">
           Jillian Meehan
         </a>{" "}
         and is {""}
-        <a
-          href="https://weather-app-react-jillian.netlify.app/"
-          target="_blank"
-        >
+        <a href="https://weather-app-react-jillian.netlify.app/">
           open-sourced on GitHub
         </a>
       </footer>
